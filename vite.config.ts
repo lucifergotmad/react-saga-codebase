6b0282200837895b3385a594ac856b55351885d7
lucifergotmad <amateur.fullstackdev@gmail.com>
2024-06-17 15:46:17 +0700

    fix:[major] obfuscate so its hard to reverse engineer

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import viteCompression from 'vite-plugin-compression';
 import vitePluginEnvirontment from 'vite-plugin-environment';
 import * as path from 'path';
 
+const timestamp = new Date().getTime();
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -24,6 +26,37 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, 'src/utils'),
       '@pages': path.resolve(__dirname, 'src/pages'),
       '@shared': path.resolve(__dirname, 'src/shared'),
+    },
+  },
+  build: {
+    minify: 'esbuild',
+    emptyOutDir: true,
+    sourcemap: false,
+    cssCodeSplit: true,
+    modulePreload: true,
+    chunkSizeWarningLimit: 1000000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+        chunkFileNames: `assets/js/[hash]-${timestamp}.js`,
+        entryFileNames: `assets/js/[hash]-${timestamp}.js`,
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg|webp|avif)$/.test(name ?? '')) {
+            return `assets/images/[hash]-${timestamp}[extname]`;
+          }
+          if (/\.(ttf|woff2|svg)$/.test(name ?? '')) {
+            return `assets/font/[hash]-${timestamp}[extname]`;
+          }
+          if (/\.css$/.test(name ?? '')) {
+            return `assets/css/[hash]-${timestamp}[extname]`;
+          }
+          return `assets/[hash]-${timestamp}[extname]`;
+        },
+      },
     },
   },
 });
